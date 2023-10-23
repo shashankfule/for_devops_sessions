77eb02cb7c24030f98d953fade0e40863546d46d
shashankfule <shashankfule143@gmail.com>
2023-10-23 08:46:09 +0530

    first push

--- node_js_file/first_prog.js
+++ node_js_file/first_prog.js
@@ -2,6 +2,7 @@ const helloStr = "Hello DevOps guys !!";
 console.log(helloStr);
 console.log("Formatting as string %s", helloStr);
 console.log(`${helloStr}`);
+console.log("hi")
 
 const duration = 11;
 console.log("Approx. duration of course in week is   %d", duration," (formatted as number)");
